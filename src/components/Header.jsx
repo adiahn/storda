365e3b58240fc9f456a7eb3de49d6a6adf7f8a64
Adiahn <adnanmukhtar2321@gmail.com>
2025-02-12 14:25:45 +0100

    Pushing Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Hero from "./Hero/Hero";
 
 const Header = () => {
   return (
-    <div className="h-[80vh] w-[100vw] text-white bg-radial-[at_25%_25%] from-cyan-900 to-cyan-950 flex flex-col items-center">
+    <div className="min-h-[80vh] w-full max-w-screen text-white bg-radial-[at_25%_25%] from-cyan-950 to-black flex flex-col items-center relative overflow-x-hidden px-1 sm:px-1 lg:px-4">
       <Navbar />
       <Hero />
     </div>
