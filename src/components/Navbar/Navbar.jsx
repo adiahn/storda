14d1beeced817477a8dbf21279e0ed1b0d1e4e88
Adiahn <adnanmukhtar2321@gmail.com>
2025-02-12 12:10:43 +0100

    Continued Progress

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center border border-white w-full p-2 h-[10vh]">
+      <h1 className="text-2xl bold uppercase">Stodar</h1>
+      <ul className=""></ul>
+    </div>
+  );
+};
+
+export default Navbar;
