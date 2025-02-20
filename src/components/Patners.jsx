6e24444727d7fbeb10859198e77a28af0be09885
Adiahn <adnanmukhtar2321@gmail.com>
2025-02-20 11:27:05 +0100

    Repush

--- src/components/Patners.jsx
+++ src/components/Patners.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Patners = () => {
-  let ourPartnerss = ['partner1', 'partner2', 'partner3', 'partner4', 'partner5']
+  let ourPartners = ['partner1', 'partner2', 'partner3', 'partner4', 'partner5']
   return (
     <div className='w-full flex flex-col items-center justify-center'>
       <p className='text-white'>Supported by</p>
       <div className='flex flex-row justify-center items-center gap-50 p-5'>
-        {ourPartnerss.map((partner) => (
+        {ourPartners.map((partner) => (
           <p className='text-white uppercase bold'> {partner} </p>
         ))}
       </div>
